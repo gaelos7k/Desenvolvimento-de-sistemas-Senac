ace736dda12d7352f85451942c9fd3e5ac9dfd2c
Gabriel Henrique da Silva Goncalves <1055458@senacminas.edu.br>
2025-08-05 11:13:13 -0300

    Atividade 04 e 05

---
+++ UC5 - Desenvolvimento desktop/python/atividades/atividade04.py
@@ -0,0 +1,9 @@
+c = 2
+
+numero = int(input('Digite um número inteiro: '))
+
+dobro = numero * 2
+metade = numero / 2
+
+print(f'O dobro é: {dobro}')
+print(f'A metade é: {metade}')
---
+++ UC5 - Desenvolvimento desktop/python/atividades/atividade05.py
@@ -0,0 +1,7 @@
+desconto = 0.9
+
+valorOriginal = float(input('Qual o valor do produto? R$'))
+
+valorFinal = valorOriginal * desconto
+
+print(f'O valor do produto com desconto é R${valorFinal}')
