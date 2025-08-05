09ae63d00506450d0814f36ba7571b8b425deb87
Gabriel Henrique da Silva Goncalves <1055458@senacminas.edu.br>
2025-08-05 11:01:22 -0300

    Atividade 02 e 03 em Python

---
+++ UC5 - Desenvolvimento desktop/python/atividades/atividade02.py
@@ -0,0 +1,8 @@
+c1 = 1.8
+c2 = 32
+
+tc = float(input('Temperatura °C: '))
+
+tf = tc * c1 + c2
+
+print(f'Temperatura F: {tf}')
---
+++ UC5 - Desenvolvimento desktop/python/atividades/atividade03.py
@@ -0,0 +1,9 @@
+n1 = float(input('Insira a nota 1: '))
+n2 = float(input('Insira a nota 2: '))
+n3 = float(input('Insira a nota 3: '))
+
+c = 3
+
+media = (n1 + n2 + n3) / c
+
+print(f'A média das notas é: {media}')
