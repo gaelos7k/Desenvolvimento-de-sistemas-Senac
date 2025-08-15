652ed8ec059bb20f5ee867518943d3a5d63af94c
Gabriel Henrique <l77gabrielgh77@hotmail.com>
2025-08-15 13:24:32 -0300

    Atividade 26

---
+++ UC5 - Desenvolvimento desktop/python/atividades/atividade26.py
@@ -0,0 +1,15 @@
+import random
+
+targetNumber = random.randint(1, 100)
+
+number = -1
+
+while number != targetNumber:
+    number = int(input('Escolha um número entre 0 e 100:'))
+
+    if number == targetNumber:
+        print('Parabéns, você acertou!')
+    elif number < targetNumber:
+        print(f'O número secreto é maior que {number}')
+    else:
+        print(f'O número secreto é menor que {number}')
