ed818b7e557fda6f6b82624b35bfe5f7746bb252
Gabriel Henrique da Silva Goncalves <1055458@senacminas.edu.br>
2025-08-18 09:14:23 -0300

    Otimização do código da atividade 29

--- UC5 - Desenvolvimento desktop/python/atividades/atividade29.py
+++ UC5 - Desenvolvimento desktop/python/atividades/atividade29.py
@@ -8,11 +8,18 @@ while True:
 
     media = (note1 + note2 + note3 + note4) / 4
 
-    while not(0 < note1 <= 10) or not(0 < note2 <= 10) or not(0 < note3 <= 10) or not(0 < note4 <= 10):
-        note1 = float(input('Informe a nota 1 entre 0 e 10 novamente: '))
-        note2 = float(input('Informe a nota 2 entre 0 e 10 novamente:'))
-        note3 = float(input('Informe a nota 3 entre 0 e 10 novamente:'))
-        note4 = float(input('Informe a nota 4 entre 0 e 10 novamente:'))
+    while not (0 <= note1 <= 10) or not (0 <= note2 <= 10) or not (0 <= note3 <= 10) or not (0 <= note4 <= 10):
+
+        if not (0 <= note1 <= 10):
+            note1 = float(input('\nInforme a nota 1 entre 0 e 10 novamente: '))
+        elif not (0 <= note2 <= 10):
+            note2 = float(input('\nInforme a nota 2 entre 0 e 10 novamente:'))
+        elif not (0 <= note3 <= 10):
+            note3 = float(input('\nInforme a nota 3 entre 0 e 10 novamente:'))
+        elif not (0 <= note4 <= 10):
+            note4 = float(input('\nInforme a nota 4 entre 0 e 10 novamente:'))
+
+        media = (note1 + note2 + note3 + note4) / 4
 
     if media > 7:
         print('Aluno aprovado')
