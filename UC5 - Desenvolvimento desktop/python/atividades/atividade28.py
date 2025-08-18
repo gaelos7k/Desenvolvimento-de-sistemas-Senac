619428916b65c17fb3a14cb74f2fddd4eb598793
Gabriel Henrique da Silva Goncalves <1055458@senacminas.edu.br>
2025-08-18 08:59:48 -0300

    Organização do código da atividade 28

--- UC5 - Desenvolvimento desktop/python/atividades/atividade28.py
+++ UC5 - Desenvolvimento desktop/python/atividades/atividade28.py
@@ -6,12 +6,12 @@ while True:
     for i in range(1, 11):
         print(f'{number} x {i} = {number * i}')
 
-    chose = input('Deseja ver outra tabuaba? ')
+    chose = input('\nDeseja ver outra tabuaba? ')
 
     if chose.lower() == 'sim':
-        number = float(input('Escolha um número para exibir sua tabuada de 1 a 10: '))
+        number = float(input('Escolha outro número para exibir sua tabuada de 1 a 10: '))
         for i in range(1, 11):
             print(f'{number} x {i} = {number * i}')
-        chose = input('Deseja ver outra tabuaba? ')
+        chose = input('\nDeseja ver outra tabuaba? ')
     else:
         break
