c7e56fa856004afc0a065c78497dccbd981104a0
Gabriel Henrique da Silva Goncalves <1055458@senacminas.edu.br>
2025-08-18 08:56:11 -0300

    Adicionado comentários a atividade 27

--- UC5 - Desenvolvimento desktop/python/atividades/atividade27.py
+++ UC5 - Desenvolvimento desktop/python/atividades/atividade27.py
@@ -1,25 +1,28 @@
 # Validador de Senha
 
 while True:
-    senha = input("Insira a senha:")
+    password = input("Insira a senha:")
 
     # Contadores
     count_alpha = 0
     count_nums = 0
 
     # Checagem de caracteres
-    for c in senha:
+    for c in password:
         if c.isalpha():
             count_alpha += 1
         elif c.isdigit():
             count_nums += 1
 
-    if len(senha) < 8:
+    # Minímo de caracteres
+    if len(password) < 8:
         print("Senha deve conter pelo menos 8 caracteres!")
 
+    # Se contador de letras for igual a 0 aparece o erro
     elif not count_alpha:
         print("Senha deve conter letras!")
 
+    # Se o contador de números for igual a 0 aparece o erro
     elif not count_nums:
         print("Senha deve conter números!")
 
