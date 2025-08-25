236f16c5e4d309b30421477461c7fa5aa4e75276
Gabriel Henrique da Silva Goncalves <1055458@senacminas.edu.br>
2025-08-25 10:05:04 -0300

    Refatorção da atividade 30 - Calculadora interativa

--- UC5 - Desenvolvimento desktop/python/atividades/atividade30.py
+++ UC5 - Desenvolvimento desktop/python/atividades/atividade30.py
@@ -3,29 +3,30 @@
 while True:
 
     print('Escolha a opção no menu para a operação que deseja realizar: ')
-    operation = input(
+    operation = int(input(
         '[1] +\n'
         '[2] -\n'
         '[3] *\n'
         '[4] /\n'
         '[5] Sair\n'
-    )
+    ))
 
-    if operation == '1' or operation == '+':
-        n1 = float(input('Escolha o primeiro número para a adição: '))
-        n2 = float(input('Escolha o segundo número para a adição: '))
-        print(f'{n1} + {n2} = {n1 + n2}\n')
-    elif operation == '2' or operation == '-':
-        n1 = float(input('Escolha o primeiro número para a subtração: '))
-        n2 = float(input('Escolha o segundo número para a subtração: '))
-        print(f'{n1} - {n2} = {n1 - n2}\n')
-    elif operation == '3' or operation == '*':
-        n1 = float(input('Escolha o primeiro número para a multiplicação: '))
-        n2 = float(input('Escolha o segundo número para a multiplicação: '))
-        print(f'{n1} * {n2} = {n1 * n2}\n')
-    elif operation == '4' or operation == '/':
-        n1 = float(input('Escolha o primeiro número para a divisão: '))
-        n2 = float(input('Escolha o segundo número para a divisão: '))
-        print(f'{n1} / {n2} = {n1 / n2}\n')
-    else:
+    if operation == 5:
         break
+    else:
+        n1 = float(input('Escolha o primeiro para realizar a operação: '))
+        n2 = float(input('Escolha o segundo para realizar a operação: '))
+
+    match operation:
+        case 1:
+            print(f'{n1} + {n2} = {n1 + n2}\n')
+        case 2:
+            print(f'{n1} - {n2} = {n1 - n2}\n')
+        case 3:
+            print(f'{n1} * {n2} = {n1 * n2}\n')
+        case 4:
+            while n2 == 0:
+                n2 = float(input('Escolha o segundo número para a divisão que seja diferente de 0: '))
+                if n2 > 0:
+                    break
+            print(f'{n1} / {n2} = {n1 / n2}\n')
