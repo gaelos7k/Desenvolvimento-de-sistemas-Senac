4a5d3513d337be6b644d58e040212a229fc35408
Gabriel Henrique da Silva Goncalves <1055458@senacminas.edu.br>
2025-08-26 08:01:59 -0300

    Melhoria na operação da fórmula de IMC

--- UC5 - Desenvolvimento desktop/python/atividades/atividade33.py
+++ UC5 - Desenvolvimento desktop/python/atividades/atividade33.py
@@ -1,10 +1,10 @@
 # Função que calcula o IMC de uma pessoa e retorna a classificação da mesma
 def imc_calculator(weight, height):
-    if weight / (height * height) < 18.5:
+    if weight / (height**2) < 18.5:
         return 'Abaixo do peso'
-    elif weight / (height * height) < 24.99:
+    elif weight / (height**2) < 24.99:
         return 'Peso normal'
-    elif weight / (height * height) < 29.99:
+    elif weight / (height**2) < 29.99:
         return 'Sobre peso'
     else:
         return 'Obesidade'
