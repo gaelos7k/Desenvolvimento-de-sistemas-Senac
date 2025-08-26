35d54455c6b2a04c4c5eedc13b43dfb4a6b484f7
Gabriel Henrique da Silva Goncalves <1055458@senacminas.edu.br>
2025-08-26 11:19:49 -0300

    Validador de CPF

---
+++ UC5 - Desenvolvimento desktop/python/atividades/atividade34.py
@@ -0,0 +1,27 @@
+def cpf_validation(cpf: str) -> str:
+    cpf_numbers = []
+
+    # Atribuindo apenas os valores numéricos ao CPF
+    for digit in cpf:
+        if digit.isdigit():
+            cpf_numbers.append(digit)
+    cpf = ''.join(cpf_numbers)
+
+    # CPF deve conter 11 digitos
+    if len(cpf) != 11:
+        return 'CPF inválido: Quantidade de digítos incorreta!'
+
+    # CPF não pode ter todos os número iguais
+    if cpf == cpf[0] * 11:
+        return "CPF inválido: sequência repetida"
+
+    # Algorítmo para fazer checagem se os dígitos verificadores são válidos
+    for i in range(9, 11):
+        soma = 0
+        for j in range(i):
+            soma = soma + int(cpf[j]) * (i + 1 - j)
+        digit = (soma * 10 % 11) % 10
+        if int(cpf[i]) != digit:
+            return "CPF inválido: digitos verificadores inválidos!"
+
+    return 'CPF válido'
