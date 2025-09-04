55e09bc235d9a34367966838d18318fe10096242
Gabriel Henrique da Silva Goncalves <1055458@senacminas.edu.br>
2025-09-04 11:21:06 -0300

    Aulas dos dia 03/09/25 e 04/09/25

--- UC5 - Desenvolvimento desktop/python/atividades/teste.py
+++ UC5 - Desenvolvimento desktop/python/atividades/teste.py
@@ -1,1 +1,18 @@
-print(bool())
+def altura_piramide(blocos: int) -> int:
+    altura = 0
+    usados = 0
+
+    while usados + (altura + 1) <= blocos:
+        altura += 1
+        usados += altura
+
+    return altura
+
+
+# Testes
+print(altura_piramide(1))  # 1 camada
+print(altura_piramide(3))  # 2 camadas (1+2 = 3)
+print(altura_piramide(5))  # 2 camadas (1+2=3, sobra 2 blocos mas não completa a 3ª)
+print(altura_piramide(6))  # 3 camadas (1+2+3=6)
+print(altura_piramide(20))  # 5 camadas (1+2+3+4+5=15, sobra 5 blocos)
+
